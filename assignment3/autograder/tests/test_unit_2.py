69c87f1e10f0f058df74d5323ae1b2f033f52eb3
Olivia Coffey <ocoffey@protonmail.com>
2019-11-20 19:34:26 -0500

    updated unit tests

--- assignment3/autograder/tests/test_unit_2.py
+++ assignment3/autograder/tests/test_unit_2.py
@@ -1,72 +1,40 @@
 import unittest
 from gradescope_utils.autograder_utils.decorators import weight
-import os
-import re
 import subprocess
+from collections import Counter
 
-
-class TestTweetReader(unittest.TestCase):
-    """Test Tweet Reader - 50 points"""
+class TestDict(unittest.TestCase):
+    """Test Dictionary Lookups - 40 points
+    
+    Test that the outputs are as they should be
+    """
 
     def setUp(self):
         self.test = 'tests/miserable.txt'
+        self.ourdict = 'tests/american-english'
         self.testpy = 'tests/testassignment3.py'
 
-    def sub_func(self, stinput: str) -> bool:
+    @weight(40)
+    def test_outputs(self):
         """Function call to actually run the test"""
         # runs the students file, passing a .txt file
-        studp = subprocess.run(['python3', 'assignment3.py', self.test], stdout=subprocess.PIPE, input=stinput, encoding='ascii')
+        studp = subprocess.run(['python3', 'assignment3.py', self.test, self.ourdict], stdout=subprocess.PIPE, encoding='ascii')
         # runs our file, passing the same .txt file
-        testp = subprocess.run(['python3', self.testpy, self.test], stdout=subprocess.PIPE, input=stinput, encoding='ascii')
+        testp = subprocess.run(['python3', self.testpy, self.test, self.ourdict], stdout=subprocess.PIPE, encoding='ascii')
         # strips students stdout of all whitespace
-        studstripped = re.sub(r"([\s\t\n])", r"", studp.stdout.strip())
+        studOutList = studp.stdout.split('\n')
         # strips our stdout of all whitespace
-        teststripped = re.sub(r"([\s\t\n])", r"", testp.stdout.strip())
+        testOutList = testp.stdout.split('\n')
         # Make sure that both returned 0
         if studp.returncode == testp.returncode and testp.returncode == 0:
-            # compares outputs and returns that bool
-            return studstripped == teststripped
+            # count the words and compare counts
+            self.assertEqual(Counter(studOutList),Counter(testOutList))
         # Let me know if I made an error
         elif testp.returncode != 0:
             print("I made an error")
-            print(self.test, end='')
-            print(" was the filename passed.")
-            return False
+            print(self.test + " was the book passed.")
+            print(self.ourdict + " was the dictionary passed.")
+            self.assertTrue(False)
         # If the student didn't return 0
         else:
-            return False
-
-
-    @weight(2)
-    def test_quit(self):
-        testq = subprocess.run(['python3', 'assignment3.py', self.test], stdout=subprocess.PIPE, input='q\n', encoding='ascii')
-        self.assertEqual(testq.returncode,0)
-        return
-    
-    @weight(4)
-    def test_next(self):
-        self.assertIs(self.sub_func('n\nq\n'),True)
-
-    @weight(4)
-    def test_prev(self):
-        self.assertIs(self.sub_func('p\nq\n'),True)
-
-    @weight(4)
-    def test_first(self):
-        self.assertIs(self.sub_func('f\nq\n'),True)
-
-    @weight(4)
-    def test_last(self):
-        self.assertIs(self.sub_func('l\nq\n'),True)
-
-    @weight(8)
-    def test_num(self):
-        self.assertIs(self.sub_func('num\nq\n'),True)
-
-    @weight(10)
-    def test_skip(self):
-        self.assertIs(self.sub_func('5000\nq\n'),True)
-
-    @weight(14)
-    def test_search(self):
-        self.assertIs(self.sub_func('s cancer\nq\n'),True)
+            self.assertTrue(False)
