c784cd46407cdde716394c24eb29938d37559b43
Olivia Coffey <ocoffey@protonmail.com>
2019-11-23 21:18:44 -0500

    key in dictionary check working

--- assignment3/autograder/tests/testassignment3.py
+++ assignment3/autograder/tests/testassignment3.py
@@ -1,47 +1,51 @@
 import sys
 import re
-"""
-Open and read in story .txt file
-"""
-# make sure we can actually open the passed filename
-try:
-    sys.argv[1]
-except IndexError:
-    print("File Not Passed")
-    sys.exit()
 
-with open(sys.argv[1],'r') as f:
-    story = f.readlines()
+def main():
+    """
+    Open and read in story .txt file
+    """
+    # make sure we can actually open the passed filename
+    try:
+        sys.argv[1]
+    except IndexError:
+        print("File Not Passed")
+        sys.exit()
 
-"""
-Open and read in dict file
-"""
+    with open(sys.argv[1],'r') as f:
+        story = f.readlines()
 
-try:
-    sys.argv[2]
-except IndexError:
-    print("Dict Not Passed")
-    sys.exit()
+    """
+    Open and read in dict file
+    """
 
-with open(sys.argv[2],'r') as f:
-    mydict = f.read().splitlines()
+    try:
+        sys.argv[2]
+    except IndexError:
+        print("Dict Not Passed")
+        sys.exit()
 
-"""
-Put dict file into dictionary
-"""
-userdict = dict.fromkeys(mydict, True)
+    with open(sys.argv[2],'r') as f:
+        mydict = f.read().splitlines()
 
-"""
-Print words from the file if they're not in the dict
-"""
-# for each line in the story
-for line in story:
-    # replace junk chars with "", split based on whitespace
-    line = re.sub(r"([^a-zA-Z0-9'])", r"", line).split()
-    # see if each word is in the dict
-    for word in line:
-        try:
-            userdict[word]
-        # if not, print it
-        except KeyError:
-            print(word)
+    """
+    Put dict file into dictionary
+    """
+    userdict = dict.fromkeys([word.lower() for word in mydict])
+
+    """
+    Print words from the file if they're not in the dict
+    """
+    # for each line in the story
+    for line in story:
+        # convert to lowercase, and split based on whitespace
+        line = line.lower().split()
+        # see if each word is in the dict
+        for word in line:
+            # remove junk chars in the word
+            word = re.sub(r"([^a-z0-9'])", r"", word)
+            if word not in userdict:
+                print(word)
+
+if __name__ == "__main__":
+    main()
